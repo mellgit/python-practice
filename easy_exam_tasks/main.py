5f4726bffa07a0c6b4464322872a31d18d90727c
mellgit <daniilhorn8@gmail.com>
2021-03-22 19:49:45 +0300

    first task

---
+++ easy_exam_tasks/main.py
@@ -0,0 +1,89 @@
+import csv
+import ice_cream.csv as ic
+
+def main():
+    
+    izi_task_1_10()
+
+
+def task_11():
+    pass
+
+
+def task_12():
+    pass
+
+
+def task_13():
+    pass
+
+def izi_task_1_10():
+    ##### 1
+    # s1 = "4.0" # done
+    # s2 = "-1" # done
+    # s3 = "ABCD"
+    # s4 = "12 000"
+    # s5 = "12,4"
+
+    # print(float(s5))
+
+    ##### 2
+    # number = input()
+    # new_number = number * int(number) # int
+    # print(type(new_number))
+
+    ##### 3
+    # while True:
+    #     mark = int(input())
+    #     if mark > 7:
+    #         print("great")
+    #     elif mark > 5:
+    #         print("good")
+    #     elif mark > 3:
+    #         print("удов")
+    #     else:
+    #         print("неудов")
+
+    ##### 4 --> 2,3
+    # e = 2
+    # if e >1:
+    #     print("гот яиш")
+    # print("кофе")
+
+    ##### 5
+    # n1 = int(input())
+    # n2 = int(input())
+    # while n1<=n2:
+    #     if n1%2==0:
+    #         print(n1**2)
+    #     n1 += 1
+
+    ##### 6
+    # shop = ('milk', 'bread', 'choco')
+    # shop_list = list(shop)
+    # shop_list[1] = 'salat'
+    # shop = tuple(shop_list)
+    # print(shop[1])
+
+    ##### 7
+    # p = 'cat dog'.split()
+    # p.append('parrot')
+    # print(p)
+
+    ##### 8 --> 1, 3, 4
+    # w = "account"
+    # print(w.rfind("ac"))
+    # print(w.find("ac"))
+
+    ##### 9 --> 0 ноль не отрицательное число
+    # s = "а роза упала на лапу азора"
+    # print(s[0:])
+
+    ##### 10
+    # st = ['mark', 'alica', 'evgen']
+    # for i in range(len(st)):
+    #     print(st[i])
+
+
+if __name__ == "__main__":
+    main()
