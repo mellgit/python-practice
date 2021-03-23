db23674698d1f69e92bc5fae251866e3363c7dc0
mellgit <daniilhorn8@gmail.com>
2021-03-23 21:08:58 +0300

    added new tasks and solutions to them

--- easy_exam_tasks/main.py
+++ easy_exam_tasks/main.py
@@ -1,21 +1,115 @@
 import csv
-import ice_cream.csv as ic
+import collections as c
+import re
 
 def main():
     
-    izi_task_1_10()
+    # izi_task_1_10()
+    # task_11()
+    # task_12() # ready
+
+    # red = [4, 5, 5, 4, 5, 5, 4]
+    # red = [4, 5, 5, 4, 5, 5, 4, 4]
+    red = [4, 5, 5, 4, 5, 5, 4, 4, 3]
+    t13 = task_13(red) # ready
+    print(t13)
 
 
+# work with csv file
 def task_11():
-    pass
+
+    # with open("ice_cream.csv", newline='', encoding='utf-8') as csv_file:
+    #     reader = csv.DictReader(csv_file, delimiter=';')
+        
+
+    with open("ice_cream.csv", encoding='utf-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=';')
+
+        counter = c.Counter()
+        for row in csvreader:
+            if row[2]:  # skip empty
+                counter[row[2]] += 1
+                
+
+    print(counter.most_common(1))
+    
+
+
+    # strawberry_count = 0
+    # mango_count = 0
+    # apple_count = 0
+
+
+
+    # with open("ice_cream.csv", newline='', encoding='utf-8') as csv_file:
+    #     reader = csv.DictReader(csv_file, delimiter=';')
+        
+    #     for row in reader:
+    #         print(row['ice_1'], row['ice_2'], row['ice_3'])
+
+
+        #     if "клубника" in row['ice_1'] or row['ice_2'] or row['ice_3']:
+        #         strawberry_count += 1
+        # print(strawberry_count)
+
+    # read file
+    # with open("ice_cream.csv", newline='', encoding='utf-8') as csv_file:
+    #     reader = csv.DictReader(csv_file, delimiter=';')
+    #     for row in reader:
+    #         print(row['ice_1'], row['ice_2'], row['ice_3'])
+    # write file
+    # with open("new_file.csv", 'w', newline='', encoding='utf-8') as csv_file:
+    #     writer = csv.writer(csv_file, delimiter=';')
+    #     writer.writerow(['row 1', 'row 2', 'row 3'])
 
 
 def task_12():
-    pass
+    s = "Pearl Jam - Garden; Fools Garden - Lemon tree;".lower().split(";")
+    word = "garden"
+    count = 0
+    print(s)
+    print(word)
+    for i in range(len(s)):
+        if word in s[i]:
+            count+=1
+    print(count)
+
+    # print(s.count(word))
+    # подсчет колличества слов в строке
+    # count = 0
+    # flag = 0
+    # count_word = 0
+    # for i in range(len(s)):
+    #     if s[i] != ' ' and flag == 0:
 
 
-def task_13():
-    pass
+    #         count += 1
+    #         flag = 1
+            
+    #     else:
+    #         if s[i] == ' ':
+    #             flag = 0
+    # print(count_word)
+    # подсчет каждого символа
+    # results = c.Counter(trek)
+    # print(results)
+    # counter = 0
+    # word = "garder"
+    # if word in trek:
+    #     print(1)
+    # else:
+    #     print(0)
+    
+
+def task_13(red):
+    
+    if 3 in red:
+        return False
+    elif red.count(4) > 3:
+        return False
+    else:
+        return True
+    
 
 def izi_task_1_10():
     ##### 1
@@ -80,9 +174,9 @@ def izi_task_1_10():
     # print(s[0:])
 
     ##### 10
-    # st = ['mark', 'alica', 'evgen']
-    # for i in range(len(st)):
-    #     print(st[i])
+    st = ['mark', 'alica', 'evgen']
+    for i in range(len(st)):
+        print(st[i])
 
 
 if __name__ == "__main__":
