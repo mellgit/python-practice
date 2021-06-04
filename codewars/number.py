515488f52db35e521f7946c88b5846cb5724bb4b
mell <daniilhorn8@gmail.com>
2021-06-05 01:39:27 +0300

    number

---
+++ codewars/number.py
@@ -0,0 +1,25 @@
+# Find the first non-consecutive number
+
+
+def main():
+    print(first_non_consecutive([-7, -6, -5, -4, 8]))
+
+def first_non_consecutive(arr):
+    
+    
+
+    for i in range(len(arr)-1):
+        if arr[i+1] - arr[i] != 1:
+            return arr[i+1]
+
+    return None
+          
+main()
+
+
+
+"""
+# if arr[i+1] - arr[i] != 1:
+        #     return arr[i+1]
+
+"""
