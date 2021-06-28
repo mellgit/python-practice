9eac84b02c416959af3a22b8e94e776850d11846
mell <daniilhorn8@gmail.com>
2021-06-29 02:43:13 +0300

    pre_v2

--- codewars/preslstent.py
+++ codewars/preslstent.py
@@ -1,6 +1,6 @@
 """
-Напишите функцию, persistenceкоторая принимает положительный параметр 
-numи возвращает его мультипликативную постоянство, то есть количество раз, 
+Напишите функцию, persistence, которая принимает положительный параметр 
+num и возвращает его мультипликативную постоянство, то есть количество раз, 
 которое вы должны умножить цифры, numпока не дойдете до единственной цифры.
 ex:
  persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
@@ -12,13 +12,46 @@ ex:
  persistence(4) # returns 0, because 4 is already a one-digit number
 """
 
+# import numpy as np
+# l = [1,2,3,4,5]
+# print(np.prod(l))
 
+import numpy as np
 def main():
     print(persistence(999))
 
 def persistence(n):
+    #v5
+    num = str(n)
+    
+    if len(num) == 1:
+        if len(num) == 1 and num.isdigit() and num > '0':
+            return 0
+        else:
+            return int(num)
+    else:
+        ln = []
+        for i in range(len(num)):
+            ln.append(int(num[i]))
+        return persistence(int(np.prod(ln)))
+    
+    #v4
+    # num = str(n)
+    # ln = []
+    # for i in range(len(num)):
+    #     ln.append(int(num[i]))
+    # return persistence(int(np.prod(ln))
+
     #v3
-    pass
+    # num = str(n)
+    # ln = []
+    # j = 1
+    # while j >= len(num):
+    #     for i in range(len(num)):
+    #         ln.append(int(num[i]))
+    #     num = np.prod(ln)
+    #     j+=1
+    # return  num
 
 
     #v2
